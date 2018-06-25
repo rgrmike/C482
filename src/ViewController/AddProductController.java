0dca56e9da0904c7039906fe8d42272e0f749619
rgrmike <rgrmike@outlook.com>
2018-06-24 18:02:12 -0700

    Completed GUI Elements

---
+++ src/ViewController/AddProductController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ViewController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author miken
+ */
+public class AddProductController implements Initializable {
+
+    @FXML
+    private Button AddProdSearchButton;
+    @FXML
+    private Button AddProdAddButton;
+    @FXML
+    private Button AddProdDeleteButton;
+    @FXML
+    private Button AddProdSaveButton;
+    @FXML
+    private Button AddProdCancelButton;
+    @FXML
+    private Label AddProdMainLabel;
+    @FXML
+    private TextField AddProdSearchTextField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void AddProdSearchButtonHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void AddProdAddButtonHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void AddProdDeleteButtonHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void AddProdSaveButtonHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void AddProdCancelButtonHandler(ActionEvent event) {
+    }
+    
+}
