8cf28598a64c2bc1e415366dda95f5de8dee1ce2
rgrmike <rgrmike@outlook.com>
2018-06-29 17:37:15 -0700

    Forms now switch between add or modify text depending on how they are launched

--- src/ViewController/AddProductController.java
+++ src/ViewController/AddProductController.java
@@ -5,14 +5,19 @@
  */
 package ViewController;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -41,7 +46,13 @@ public class AddProductController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // If addmodprod = 1 then add - if it = 2 then modify
+        if(MainScreenController.addmodprod==2){
+            AddProdMainLabel.setText("Modify Product");
+        }
+        if(MainScreenController.addmodprod==1){
+            AddProdMainLabel.setText("Add Product");
+        }
     }    
 
     @FXML
@@ -57,11 +68,31 @@ public class AddProductController implements Initializable {
     }
 
     @FXML
-    private void AddProdSaveButtonHandler(ActionEvent event) {
+    private void AddProdSaveButtonHandler(ActionEvent event) throws IOException{
+        Stage stage; 
+        Parent root;
+        //get reference to the button's stage         
+        stage=(Stage) AddProdSaveButton.getScene().getWindow();
+        //load up OTHER FXML document
+        root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        //create a new scene with root and set the stage
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    private void AddProdCancelButtonHandler(ActionEvent event) {
+    private void AddProdCancelButtonHandler(ActionEvent event) throws IOException{
+        Stage stage; 
+        Parent root;
+        //get reference to the button's stage         
+        stage=(Stage) AddProdCancelButton.getScene().getWindow();
+        //load up OTHER FXML document
+        root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        //create a new scene with root and set the stage
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     
 }
