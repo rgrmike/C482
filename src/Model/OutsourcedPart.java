b1c6fa7fee6c72c030887b8a9a52787819c996c5
rgrmike <rgrmike@outlook.com>
2018-08-05 17:08:21 -0700

    Created InhousePart class - have not tested.

--- src/Model/OutsourcedPart.java
+++ src/Model/OutsourcedPart.java
@@ -5,10 +5,28 @@
  */
 package Model;
 
+import javafx.beans.property.SimpleStringProperty;
+
 /**
  *
  * @author mian
  */
-public class OutsourcedPart {
+public class OutsourcedPart extends Part {
     
+    private final SimpleStringProperty PartCompanyName= new SimpleStringProperty("");
+    public OutsourcedPart(){
+        this("");
+    }
+
+    public OutsourcedPart(String companyName) {
+        setCompanyName(companyName);
+    }
+    
+    public String getCompanyName(){
+        return PartCompanyName.get();
+    }
+    
+    public void setCompanyName(String companyName) {
+        PartCompanyName.set(companyName);
+    } 
 }
