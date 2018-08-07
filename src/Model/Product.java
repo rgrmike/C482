1c6958259540eed8158a1eaf28291e326c4d6a58
rgrmike <rgrmike@outlook.com>
2018-08-07 12:23:08 -0700

    Finished basic addpart form - no error handling - have not tested.

--- src/Model/Product.java
+++ src/Model/Product.java
@@ -5,10 +5,27 @@
  */
 package Model;
 
+import com.sun.javafx.collections.ObservableSequentialListWrapper;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import static javafx.collections.FXCollections.observableArrayList;
+import javafx.collections.ObservableList;
+
 /**
  *
  * @author mian
  */
 public class Product {
+    
+    private static ObservableList<Part> partList = observableArrayList();
+    private final SimpleIntegerProperty ProductProductID=new SimpleIntegerProperty(0);
+    private final SimpleStringProperty ProductName= new SimpleStringProperty("");
+    private final SimpleDoubleProperty ProductPrice=new SimpleDoubleProperty(0.0);
+    private final SimpleIntegerProperty ProductInStock=new SimpleIntegerProperty(0);
+    private final SimpleIntegerProperty ProductMin=new SimpleIntegerProperty(0);
+    private final SimpleIntegerProperty ProductMax=new SimpleIntegerProperty(0);
+    //public Product(){
+    //    this(partList,0,"",0.0,0,0,0);
+    //}
     
 }
